50e78d57168842eb01005df4a4c25921945e03db
Julian Garcia <garcia.j.julian@gmail.com>
2020-09-06 21:41:06 -0500

    revised

--- cars.py
+++ cars.py
@@ -41,3 +41,31 @@ print(showroom)
 
 showroom.discard('Camaro')
 print(showroom)
+
+
+# Now create another set of cars in a variable junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the showroom set.
+
+junkyard = set()
+
+junkyard = {'Ford', 'Honda', 'Kia', 'Scion', 'Mustang', 'Porche'}
+
+# Use the intersection method to see which cars exist in both the showroom and that junkyard.
+
+dup_validator = junkyard.intersection(showroom)
+print(dup_validator)
+
+# Now you're ready to buy the cars in the junkyard. Use the union method to combine the junkyard into your showroom.
+
+junkyard = junkyard.union(showroom)
+print(junkyard)
+
+# Use the discard() method to remove any cars that you acquired from the junkyard that you do not want in your showroom.
+
+# ONE METHOD CAN BE THIS WAY WITH Discard()
+# junkyard.discard('Ford')
+# junkyard.discard('Honda')
+# junkyard.discard('Kia')
+
+# LEARNED THIS IS COOL WAY TO DO MULTIPLE OBJECTS AT ONCE difference_update()
+junkyard.difference_update({'Ford', 'Honda', 'Kia'})
+print(junkyard)
